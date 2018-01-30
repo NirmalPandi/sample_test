2894ccdbf774d2024467194775727b298ae6d8d1
balajcpenney <33097761+balajcpenney@users.noreply.github.com>
2018-01-30 15:01:53 +0530

    storybook sample

---
+++ storybook-react/storybook/components/Button.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+
+export default class Button extends Component{
+
+
+    render(){
+        return(
+            <div>
+                <button>click me..!</button>
+            </div>
+        )
+    }
+}
